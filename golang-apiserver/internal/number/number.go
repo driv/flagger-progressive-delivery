f7529edfb6484edbf6229e34224c42131417a3a9
Federico Nafria <federiconafria@gmail.com>
2024-07-17 09:00:34 +0200

    Speed up fibonacci

--- golang-apiserver/internal/number/number.go
+++ golang-apiserver/internal/number/number.go
@@ -12,6 +12,18 @@ func Fibonacci(n int) int {
 	if n <= 1 {
 		return n
 	}
+	prev, curr := 0, 1
+	for i := 1; i < n; i++ {
+		prev, curr = curr, prev+curr
+	}
+	return curr
+	// return Fibonacci(n-1) + Fibonacci(n-2)
+}
+
+func SlowFibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
 	return Fibonacci(n-1) + Fibonacci(n-2)
 }
 
